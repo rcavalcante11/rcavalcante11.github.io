ace5cf96fbeeb96debfabcc7942ec8bdc1cda45a
Rafael Cavalcante <dicavalcante18@gmail.com>
2024-04-05 22:28:34 -0300

    navbar check and background

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-let hamburguer = document.querySelector(".hamburguer");
-let navLinks = document.getElementById(".nav-links");
-let links = document.querySelectorAll(".links");
+let hamburger = document.querySelector('.hamburger');
+let navLinks = document.getElementById('nav-links');
+let links = document.querySelectorAll('.links');
 
-//display elements on the hamburguer
-hamburguer.addEventListener("click", () => {
-  navLinks.classList.toggle("hide");
-  hamburguer.classList.toggle("lines-rotate");
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('hide');
+  hamburger.classList.toggle('lines-rotate');
 });
 
-//hide navlink container onclick any single link
 for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", () => {
-    navLinks.classList.toggle("hide");
+  links[i].addEventListener('click', () => {
+    navLinks.classList.toggle('hide');
   });
 }
